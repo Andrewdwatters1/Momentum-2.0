9905ae01c1d9b208ba33a871cf2234997bdb9ce0
Andrew Watters <Andrewdwatters1@gmail.com>
2018-08-16 17:36:35 -0600

    Cursor toggle working, clicking anywhere on image triggers modal

--- src/components/ComboItem.js
+++ src/components/ComboItem.js
@@ -6,6 +6,8 @@ class ComboItem extends Component {
     super(props)
     this.state = {
       open: false,
+      modalButtonActive: false,
+      cursorSelect: false,
     }
   }
 
@@ -15,19 +17,32 @@ class ComboItem extends Component {
   onCloseModal = () => {
     this.setState({ open: false });
   };
+  toggleStyle = (e) => {
+    e.target.classList.toggle('magnify')
+  }
 
   render() {
     const { open } = this.state;
     return (
       <div className="quotes-grid-item">
-        <img src={this.props.imgsrc} alt="Oops, something went wrong :(" className="quotes-grid-image"/>
-        <div className="quotes-grid-modal-button">
-        <button onClick={this.onOpenModal} className="quotes-grid-modal-button">+</button>
-        </div>
+        <img
+          src={this.props.imgsrc}
+          alt="Oops, something went wrong :("
+          className="quotes-grid-image"
+          onMouseEnter={() => this.setState({ modalButtonActive: true })}
+          onMouseLeave={() => this.setState({ modalButtonActive: false })}
+          onMouseEnter={this.toggleStyle}
+          onMouseLeave={this.toggleStyle}
+          onMouseDown={this.onOpenModal}
+        />
         <Modal open={open} onClose={this.onCloseModal} center>
           Testing
         </Modal>
-        <div className="quotes-grid-text">{this.props.quote}</div>
+        <div
+          className="quotes-grid-text"
+          onMouseEnter={() => this.setState({ modalButtonActive: true })}
+          onMouseLeave={() => this.setState({ modalButtonActive: false })}
+        >{this.props.quote}</div>
       </div>
     )
   }
